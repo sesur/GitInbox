ba666cb326179b729100647f516849502cc9a911
Sergiu Suru <sergiu.suru@travelport.com>
2023-12-19 12:36:04 +0200

    New Xcode project Interface: SwiftUI, Language: Swift: Unit Tests on

---
+++ GitInbox/GitInboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitInboxApp.swift
+//  GitInbox
+//
+//  Created by Sergiu Suru on 19/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct GitInboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

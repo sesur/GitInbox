ae6dcd7de4dcb565da35e70a7466673fe6303b24
Sergiu Suru <sergiu.suru@travelport.com>
2023-12-19 16:25:38 +0200

    Add a template test demo

---
+++ GitInboxTests/FirstTests.swift
@@ -0,0 +1,9 @@
+@testable import GitInbox
+import XCTest
+
+@MainActor
+final class FirstTests: XCTestCase {
+    func test_zero() throws {
+//        XCTFail("Tests not yet implemented in FirstTests")
+    }
+}

667977f1d481d93e229682e21e786f1d06dc8e68
Sergiu Suru <sergiu.suru@travelport.com>
2023-12-20 00:17:04 +0200

    R! Change app entry point to Main

---
+++ GitInbox/Main.swift
@@ -0,0 +1,16 @@
+//
+//  Main.swift
+//  GitInbox
+//
+//  Created by Sergiu Suru on 20/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct Main {
+    static func main() {
+        GitInboxApp.main()
+    }
+}
+
